39654976ee598da6575c79be317beec6f101174a
alexander78 <alexander78@list.ru>
2019-11-04 13:52:52 +0300

    not exists

---
+++ DWSIM.Math.DotNumerics/ONIT.ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    using System.Runtime.CompilerServices;
+    public static class NumericExtensionMethods
+    {
+        /// <summary>
+        /// Только цифры в строке
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsDigitsOnly(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return false;
+
+            int y = 0;
+            foreach (byte code in value)
+            {
+                if (code < 48/*0*/ || code > 57/*9*/) return false;
+            }
+
+            return true;
+        }
+    }
+}
