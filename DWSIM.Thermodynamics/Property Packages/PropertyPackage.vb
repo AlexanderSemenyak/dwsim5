bc03f0256e47c3d59b7bfb6b5a2d3e6a002ec1b3
alexander78 <alexander78@list.ru>
2021-01-17 09:33:40 +0300

    fix

--- DWSIM.Thermodynamics/Property Packages/PropertyPackage.vb
+++ DWSIM.Thermodynamics/Property Packages/PropertyPackage.vb
@@ -12176,7 +12176,7 @@ Final3:
                                              End Sub)
             container2.CreateAndAddLabelAndButtonRow("View SI Units for Phase Properties", "View Units", Nothing,
                                                      Sub(sender, e)
-                                                         Process.Start("https://github.com/DanWBR/dwsim5/blob/master/DWSIM.SharedClasses/UnitsOfMeasure/SystemsOfUnits.vb#L267")
+                                                         Process.Start("https://github.com/AlexanderSemenyak/dwsim5/blob/windows/DWSIM.SharedClasses/UnitsOfMeasure/SystemsOfUnits.vb#L290")
                                                      End Sub)
 
             Dim form = sui.GetDefaultTabbedForm("Advanced Property Package Settings", 700, 600, {container1, container2})
